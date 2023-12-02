abe33654c8af7590d303aa75117e292c549446a9
Clément NICOLAS <racacax@gmail.com>
2023-12-02 17:26:11 +0100

    add Teleboy + big logo update + update telerama

--- src/Component/Provider/Tebeosud.php
+++ src/Component/Provider/Tebeosud.php
@@ -20,16 +20,10 @@ use racacax\XmlTv\ValueObject\Program;
  */
 class Tebeosud extends AbstractProvider implements ProviderInterface
 {
-    /**
-     * @var \DateTimeZone
-     */
-    private $timezone;
 
     public function __construct(Client $client, ?float $priority = null)
     {
         parent::__construct($client, ResourcePath::getInstance()->getChannelPath('channels_tebeosud.json'), $priority ?? 0.2);
-
-        $this->timezone = new \DateTimeZone('Europe/Paris');
     }
 
     public function constructEPG(string $channel, string $date)
@@ -38,89 +32,30 @@ class Tebeosud extends AbstractProvider implements ProviderInterface
         if (!$this->channelExists($channel)) {
             return false;
         }
-        if ($date != date('Y-m-d')) {
+        $res1 = $this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date))) ?? "";
+        $res1 = str_replace('"', "'", $res1);
+        preg_match_all("/<p class='hour-program'>(.*?)<\/p>/s", $res1, $hours);
+        preg_match_all("/<span class='video-card-date'>(.*?)<\/span>/s", $res1, $titles);
+        preg_match_all("/<div class='program-card-content'> <img .*?src='(.*?)'.*?>/s", $res1, $images);
+        preg_match_all("/<p class='programm-card-duree'>.*?<span>(.*?)<\/span>.*?<\/p>/s", $res1, $durations);
+        if(count($titles[1]) == 0) {
             return false;
         }
-
-        $res1 = $this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date)));
-
-        $firstPartBegin = strpos($res1, '<span class="rouge">Programme</span>');
-        if ($firstPartBegin === false) {
-            return false;
-        }
-        $firstPartEnd = $secondPartBegin = strpos($res1, '<h3 class="grid_16 titre">');
-        $secondPartEnd = strpos($res1, '<!-- Fin liste des programmes -->');
-
-        $part1 = substr($res1, $firstPartBegin, $firstPartEnd - $firstPartBegin);
-        $part2 = substr($res1, $secondPartBegin, $secondPartEnd - $secondPartBegin);
-
-
-        $firstDay = $this->getStartDate($part1);
-        $date = (new \DateTimeImmutable($firstDay))->setTimezone($this->timezone);
-        //construct guide
-        $guide = [];
-        $previous = null;
-        foreach ([$part1, $part2] as $index => $content) {
-            $programDay = $date->modify("+$index days");
-            $listProgram = explode('<tr>', $content);
-            if (0 === $index) {
-                array_shift($listProgram);
+        for($i=0; $i < count($titles[1]); $i++) {
+            $start = strtotime($date." ".$hours[1][$i]);
+            if($i == count($titles) - 1) {
+                $end = $start + intval(explode(":", end($durations[1]))[0]) * 60;
+            } else {
+                $end = strtotime($date." ".$hours[1][$i + 1]);
             }
-            foreach ($listProgram as $programContent) {
-                $matches = [];
-                $re = '/class="date"><a href="(.*)">([\d:]+)[\s\S]*nom"><a href="(.*)">(.*)<\/a>/m';
-                preg_match($re, $programContent, $matches, PREG_OFFSET_CAPTURE, 0);
-                if (empty($matches)) {
-                    continue;
-                }
-                $url = $matches[1][0] ?? $matches[3][0];
-                if (strpos($url, 'http') !== 0) {
-                    $url = 'https:'.$url;
-                }
-                [$hour, $min] = explode(':', $matches[2][0]);
-                $begin = $programDay->setTime(intval($hour), intval($min), 0);
-                $guide[$begin->format('c')] = [
-                    'url' => $url,
-                    'begin' => $begin,
-                    'title' => $matches[4][0],
-                ];
-                if (null !== $previous) {
-                    $guide[$previous->format('c')]['end'] = $begin;
-                }
-                $previous = $begin;
-            }
-        }
-        $request = [];
-        // Convert data to Program
-        foreach ($guide as $dataProgram) {
-            if (empty($dataProgram['end'])) {
-                continue;
-            }
-            $request[] = $this->client
-                ->getAsync($dataProgram['url'])
-                ->then(function (Response $response) use ($dataProgram, $channelObj) {
-                    $content = $response->getBody()->getContents();
-                    preg_match('/"description" content="([^"]*)/m', $content, $desc);
-                    preg_match('/meta property="og:image" content="(.*?)"/', $content, $img);
+            $program = new Program($start, $end);
+            $program->addTitle(trim($titles[1][$i]));
+            $program->addDesc('Aucune description');
+            $program->setIcon($images[1][$i]);
+            $program->addCategory("Inconnu");
 
-                    $urlImage = $img[1] ?? '';
-                    if (strpos($urlImage, 'http') !== 0) {
-                        $urlImage = 'https:'.$urlImage;
-                    }
-                    $program = new Program($dataProgram['begin'], $dataProgram['end']);
-                    $program->addTitle($dataProgram['title']);
-                    $program->addDesc(trim($desc[1] ?? ''));
-                    $program->setIcon($urlImage);
-                    //$program->addCategory($program['genre']);
-
-                    $channelObj->addProgram($program);
-                }, function (\Throwable $e) use ($dataProgram, $channelObj) {
-                    $program = new Program($dataProgram['begin'], $dataProgram['end']);
-                    $program->addTitle($dataProgram['title']);
-                    $channelObj->addProgram($program);
-                });
+            $channelObj->addProgram($program);
         }
-        Utils::all($request)->wait();
 
         $channelObj->orderProgram();
 
@@ -129,30 +64,7 @@ class Tebeosud extends AbstractProvider implements ProviderInterface
 
     public function generateUrl(Channel $channel, \DateTimeImmutable $date): string
     {
-        $channel_id = $this->channelsList[$channel->getId()];
 
-        return "https://www.tebe$channel_id.bzh/le-programme";
-    }
-
-    public function getStartDate(string $content): string
-    {
-        $matches = [];
-        $re = '/<h2>[\w]+\s+([\d]+)/';
-        preg_match($re, $content, $matches, PREG_OFFSET_CAPTURE, 0);
-        if (!isset($matches[1][0])) {
-            return '';
-        }
-        $day = intval($matches[1][0]);
-        $currentDay = intval(date('d'));
-
-        if ($day === $currentDay) {
-            $startDate = date('Y-m-d');
-        } elseif ($day === intval(date('d', strtotime('-1 days')))) {
-            $startDate = date('Y-m-d', strtotime('-1 days'));
-        } else {
-            return '';
-        }
-
-        return $startDate;
+        return "https://www.tebeo.bzh/programme/".($date->format("d-m-Y"))."/";
     }
 }
---
+++ src/Component/Provider/Teleboy.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace racacax\XmlTv\Component\Provider;
+
+use _PHPStan_3bfe2e67c\Nette\Neon\Exception;
+use GuzzleHttp\Client;
+use racacax\XmlTv\Component\ProviderInterface;
+use racacax\XmlTv\Component\ResourcePath;
+use racacax\XmlTv\ValueObject\Channel;
+use racacax\XmlTv\ValueObject\Program;
+
+class Teleboy extends AbstractProvider implements ProviderInterface
+{
+    private static $API_KEY = '';
+
+    public function __construct(Client $client, ?float $priority = null)
+    {
+        parent::__construct($client, ResourcePath::getInstance()->getChannelPath('channels_teleboy.json'), $priority ?? 0.4);
+    }
+
+
+    public function constructEPG(string $channel, string $date)
+    {
+        $channelObj = parent::constructEPG($channel, $date);
+        if (!$this->channelExists($channel)) {
+            return false;
+        }
+
+        $content = $this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date)),
+            ["x-teleboy-apikey" => $this->getAPIKey()]);
+        $json = json_decode($content, true);
+        if(empty($json["data"]["items"])) {
+            return false;
+        }
+        foreach($json["data"]["items"] as $item) {
+            $programObj = new Program(strtotime($item["begin"]), strtotime($item['end']));
+            $programObj->addTitle($item['title']);
+            if(!empty($item['subtitle'])) {
+                $programObj->addSubtitle($item['subtitle']);
+            }
+            $programObj->addDesc(@$item['short_description'] ?? "Aucune description");
+            $programObj->addCategory(@$item['genre']['name_fr'] ?? "Inconnu");
+            if(!empty($item['primary_image'])) {
+                $programObj->setIcon($item['primary_image']['base_path'].'raw/'.$item['primary_image']['hash'].".jpg");
+            }
+            $channelObj->addProgram($programObj);
+        }
+
+
+
+        return $channelObj;
+    }
+
+    public function getAPIKey() {
+        if(self::$API_KEY == "") {
+            $content = $this->getContentFromURL("https://www.teleboy.ch/fr/");
+            $key = explode("'", explode('tvapiKey:', $content)[1])[1];
+            if(empty($key)) {
+                throw new Exception("API Error");
+            }
+            self::$API_KEY = $key;
+        }
+        return self::$API_KEY;
+    }
+
+    public function generateUrl(Channel $channel, \DateTimeImmutable $date): string
+    {
+        $date_start = $date->format('Y-m-d+00:00:00');
+        $date_end = $date->format('Y-m-d+23:59:59');
+        $channelId = $this->channelsList[$channel->getId()];
+        return "https://api.teleboy.ch/epg/broadcasts?begin=${date_start}&end=${date_end}&expand=flags,primary_image,genre,short_description&limit=9999&skip=0&sort=station&station=${channelId}";
+    }
+}
