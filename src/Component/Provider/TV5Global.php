7e37be8eaf8eb72dde38f55afdb561b2d0c10e6e
racacax <racacax@gmail.com>
2024-03-18 15:11:37 +0100

    fix TV5Global

--- src/Component/Provider/TV5Global.php
+++ src/Component/Provider/TV5Global.php
@@ -16,7 +16,6 @@ use racacax\XmlTv\ValueObject\Program;
  */
 class TV5Global extends AbstractProvider implements ProviderInterface
 {
-    private static $cache = array();
     public function __construct(Client $client, ?float $priority = null)
     {
         parent::__construct($client, ResourcePath::getInstance()->getChannelPath('channels_tv5global.json'), $priority ?? 0.6);
@@ -29,23 +28,35 @@ class TV5Global extends AbstractProvider implements ProviderInterface
         if (!$this->channelExists($channel)) {
             return false;
         }
-        if(!isset(self::$cache[$channel])) {
-            $content = $this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date)));
-            preg_match_all("/class=\"grid-item-time\" data-startDateTime=\"(.*?)\" data-endDateTime=\"(.*?)\" data-time-zone=\"(.*?)\".*?<h2 class=\"grid-item-title\">.*?\">(.*?)<\/a>.*?<p class=\"grid-item-categ\">(.*?)<\/p>/s", $content, $parsedData);
-            self::$cache[$channel] = $parsedData;
-        }
-        $dateTS = strtotime($date);
-        for($i=0; $i < count(self::$cache[$channel][0]); $i++) {
-            date_default_timezone_set(self::$cache[$channel][3][$i]);
-            $dateEnd = strtotime(self::$cache[$channel][2][$i]);
-            $dateStart = strtotime(self::$cache[$channel][1][$i]);
-            if($dateEnd >= $dateTS && $dateStart <= $dateTS + 86400) {
-                $program = new Program($dateStart, $dateEnd);
-                $program->addTitle(self::$cache[$channel][4][$i]);
-                $program->addDesc('Pas de description'); // TODO. Add possibility to get description
-                $program->addCategory(self::$cache[$channel][5][$i]);
-                $channelObj->addProgram($program);
+        $content = html_entity_decode($this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date))));
+        $programs = explode('<li class="hourly-list', $content);
+        unset($programs[0]);
+        $timezone = explode('<div class="filter-country select">', $content)[1];
+        $timezone = explode('<p>', $timezone)[1];
+        $timezone = explode(" ", $timezone)[1];
+        foreach ($programs as $p) {
+            preg_match('/<p class="time-start">(.*?)<\/p>/s', $p, $startTime);
+            preg_match('/<p class="time-duration">(.*?)<\/p>/s', $p, $duration);
+            preg_match('/<p class="program-type">(.*?)<\/p>/s', $p, $genre);
+            preg_match('/<h4 class="program-title">(.*?)<\/h4>/s', $p, $title);
+            preg_match('/<p class="program-summary">(.*?)<\/p>/s', $p, $summary);
+            preg_match('/src="(.*?)"/', $p, $image);
+            $startDate = strtotime($date . " " . $startTime[1] . $timezone);
+            if (str_contains($duration[1], "mn")) {
+                $duration[1] = intval(explode("mn", $duration[1])[0]) * 60;
+            } else {
+                $duration[1] = explode("h", $duration[1]);
+                $duration[1] = intval($duration[1][0]) * 3600 + intval($duration[1][1]) * 60;
             }
+            $endDate = $startDate + $duration[1];
+            $summary[1] = str_replace('<br />', '', $summary[1] ?? "");
+            $program = new Program($startDate, $endDate);
+            $program->addTitle($title[1] ?? "Aucun titre");
+            $program->addDesc($summary[1]);
+            $program->addCategory($genre[1] ?? "Inconnu");
+            if(!empty($image))
+                $program->setIcon($image[1]);
+            $channelObj->addProgram($program);
         }
         return $channelObj;
     }
@@ -54,6 +65,6 @@ class TV5Global extends AbstractProvider implements ProviderInterface
     {
         $channel_id = $this->channelsList[$channel->getId()];
 
-        return 'https://www.tv5monde.com/programmes/fr/'.$channel_id.'/';
+        return 'https://www.tv5monde.com/tv/programmes/'.$channel_id.'?day='.$date->format("Y-m-d");
     }
 }
