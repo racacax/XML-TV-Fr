0c69405b031d1e2cbad7da592d13e32ca3bb7c84
racacax <racacax@gmail.com>
2024-06-12 19:53:14 +0200

    fix tv5global

--- src/Component/Provider/TV5Global.php
+++ src/Component/Provider/TV5Global.php
@@ -14,6 +14,7 @@ use racacax\XmlTv\ValueObject\Program;
  * @author Racacax
  * @version 0.1 : 25/02/2023
  */
+
 class TV5Global extends AbstractProvider implements ProviderInterface
 {
     public function __construct(Client $client, ?float $priority = null)
@@ -28,14 +29,28 @@ class TV5Global extends AbstractProvider implements ProviderInterface
         if (!$this->channelExists($channel)) {
             return false;
         }
-        $content = html_entity_decode($this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date))));
+
+        $content = html_entity_decode($this->getContentFromURL($this->generateUrl($channelObj, new \DateTimeImmutable($date)), [
+            "Host" => "www.tv5monde.com",
+            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language" => "fr-FR,fr-CA;q=0.8,en;q=0.5,en-US;q=0.3",
+            "DNT" => "1",
+            "Sec-GPC" => "1",
+            "Connection" => "keep-alive",
+            "Upgrade-Insecure-Requests" => "1",
+            "Sec-Fetch-Dest" => "document",
+            "Sec-Fetch-Mode" => "navigate",
+            "Sec-Fetch-Site" => "none",
+            "Sec-Fetch-User" => "?1",
+            "Priority" => "u=1"
+        ]));
         $programs = explode('<li class="hourly-list', $content);
         unset($programs[0]);
         $timezone = explode('<div class="filter-country select">', $content)[1];
-        $timezone = explode('<p>', $timezone)[1];
-        $timezone = explode(" (", $timezone)[0];
-        $timezone = explode(" ", $timezone);
-        $timezone = end($timezone);
+        $timezone = explode('</li>', explode('class="active"', $timezone)[1])[0];
+        $timezone = explode('"', explode('selected-timezone=', $timezone)[1])[0];
+        date_default_timezone_set($timezone);
         foreach ($programs as $p) {
             preg_match('/<p class="time-start">(.*?)<\/p>/s', $p, $startTime);
             preg_match('/<p class="time-duration">(.*?)<\/p>/s', $p, $duration);
@@ -43,7 +58,7 @@ class TV5Global extends AbstractProvider implements ProviderInterface
             preg_match('/<h4 class="program-title">(.*?)<\/h4>/s', $p, $title);
             preg_match('/<p class="program-summary">(.*?)<\/p>/s', $p, $summary);
             preg_match('/src="(.*?)"/', $p, $image);
-            $startDate = strtotime($date . " " . $startTime[1] . $timezone);
+            $startDate = strtotime($date . " " . $startTime[1]);
             if (str_contains($duration[1], "mn")) {
                 $duration[1] = intval(explode("mn", $duration[1])[0]) * 60;
             } else {
@@ -56,7 +71,7 @@ class TV5Global extends AbstractProvider implements ProviderInterface
             $program->addTitle($title[1] ?? "Aucun titre");
             $program->addDesc($summary[1]);
             $program->addCategory($genre[1] ?? "Inconnu");
-            if(!empty($image))
+            if (!empty($image))
                 $program->setIcon($image[1]);
             $channelObj->addProgram($program);
         }
@@ -67,6 +82,6 @@ class TV5Global extends AbstractProvider implements ProviderInterface
     {
         $channel_id = $this->channelsList[$channel->getId()];
 
-        return 'https://www.tv5monde.com/tv/programmes/'.$channel_id.'?day='.$date->format("Y-m-d");
+        return 'https://www.tv5monde.com/tv/programmes/' . $channel_id . '?day=' . $date->format("Y-m-d");
     }
 }
